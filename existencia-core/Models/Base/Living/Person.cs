54e1c1e0b0b3ffe0fcc8eb1c086a715cc9815fa1
Emre Yılmaz <emreyilmaz@usta.com.tr>
2023-12-14 10:56:20 +0300

    init

---
+++ existencia-core/Models/Base/Living/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace existencia_core.Models.Base.Living;
+
+public class Person
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Gender { get; set; }
+}
---
+++ existencia-core/Models/ExistenciaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace existencia_core.Models;
+
+public class ExistenciaDbContext : DbContext
+{
+    public ExistenciaDbContext(DbContextOptions options) : base(options) { }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+        _ = configurationBuilder.Properties<DateTime>().HaveColumnType("timestamp");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+}
